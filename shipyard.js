6801fdb7441312f780e529e53d6944d9b05bd457
CutelilCucumber <sond.brian@gmail.com>
2025-10-07 17:12:40 -0700

    complete tile and board constructors yet to test

--- shipyard.js
+++ shipyard.js
@@ -1,3 +1,31 @@
+export function Gameboard(){
+    let _board = initBoard();
+
+    function initBoard() {
+        let arr = [];
+        for(let i = 0; i < 10; i++) {
+            arr[i] = [];
+            for(let j = 0; j < 10; j++) {
+                arr[i][j] = Tile();
+            }
+        }
+        return arr;
+    }
+
+    const placeShip = (x, y, ship) => {
+        _board[x][y].setShip(ship);
+    }
+
+    const receiveAttack = (x, y) => {
+        return _board[x][y].target();
+    }
+
+    return {
+        placeShip,
+        receiveAttack
+    }
+}
+
 export function Ship(length){
     let _length = length;
     let _hits = 0;
@@ -18,11 +46,25 @@ export function Ship(length){
     }
 }
 
-export function Gameboard(){
-    let _gameboard = [9][9];
-    
-    const receiveAttack = (x, y) => {
-        
+function Tile(){
+    let _hit = false;
+    let _ship = null;
+
+    const setShip = (ship) => {
+        if (_ship) throw new Error("Ship already exists at specified location.")
+        _ship = ship;
     }
 
+    const target = () => {//returns true if sunk, false if hit, 'miss' if miss
+        if (_hit) throw new Error("Location has already been targeted.")
+        _hit = true;
+
+        if (_ship) return _ship.hit();
+        else return("miss");
+    }
+
+    return {
+        setShip,
+        target
+    }
 }
