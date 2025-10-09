ff3cbc78c107c3aa6acb193063629dec3f418e4e
CutelilCucumber <sond.brian@gmail.com>
2025-10-08 22:09:03 -0700

    more interface methods and event listeners for tar

--- interface.js
+++ interface.js
@@ -1,8 +1,11 @@
 export function drawBoard(name) {
     let _name = name;
 
-    initialize()
-    function initialize(){
+    const initialize = () => {
+        let newContainer = document.createElement("div");
+        newContainer.id = _name+"Container"
+        newContainer.classList.add("boardContainer")
+        document.body.appendChild(newContainer)
         //initialize opponent board
         let newBoard = document.createElement("div");
         let newDiv = document.createElement("h3");
@@ -16,33 +19,83 @@ export function drawBoard(name) {
                 newDiv = document.createElement("div");
                 if(i < 0) newDiv.textContent = j;
                 else if (j < 0) newDiv.textContent = i;
-                else newDiv.classList.add("playable")
+                else newDiv.classList.add("target")
                 newDiv.classList.add("tile")
-                newDiv.id = j+" "+i;
+                newDiv.id = j+""+i+"opponent";
                 newBoard.appendChild(newDiv);
             }
         }
-        document.getElementById("boardContainer").appendChild(newBoard);
+        newContainer.appendChild(newBoard);
         //initialize player board
         newBoard = document.createElement("div");
         newDiv = document.createElement("h3");
         newDiv.classList.add("boardTitle")
         newDiv.textContent = _name+"'s Board";
         newBoard.appendChild(newDiv);
-        newBoard.classList.add("board")
+        newBoard.classList.add("board");
         newBoard.id = _name+"Board";
         for(let i = -1; i < 10; i++){
             for(let j = -1; j < 10; j++){//populate l -> r: j:x/ i:y
                 newDiv = document.createElement("div");
                 if(i < 0) newDiv.textContent = j;
                 else if (j < 0) newDiv.textContent = i;
-                else newDiv.classList.add("playable")
-                newDiv.classList.add("tile")
-                newDiv.id = j+" "+i;
+                else newDiv.classList.add("placeable");
+                newDiv.classList.add("tile");
+                newDiv.id = j+""+i+_name;
                 newBoard.appendChild(newDiv);
             }
         }
-        document.getElementById("boardContainer").appendChild(newBoard);
+        newContainer.appendChild(newBoard);
     }
 
+    const addShip = (x, y, shipID) => {
+        let targetTile = document.getElementById(x+''+y+_name);
+        targetTile.classList.remove("placeable");
+        targetTile.textContent = shipID;
+    }
+
+    const finishPlacement = () => {
+        document.querySelectorAll('placeable').forEach(tile => {
+            tile.classList.remove('placeable');
+        });
+        document.getElementById("opponentBoard").style.display = "grid";
+    }
+
+    const hitOpponent = (x, y) => {
+        let targetTile = document.getElementById(x+''+y+'opponent');
+        targetTile.textContent = "X";
+        targetTile.style.color = "red";
+        targetTile.classList.remove("target");
+    }
+
+    const missOpponent = (x, y) => {
+        let targetTile = document.getElementById(x+''+y+'opponent');
+        targetTile.textContent = "X";
+        targetTile.style.color = "white";
+        targetTile.classList.remove("target");
+    }
+
+    const hitSelf = (x, y) => {
+        let targetTile = document.getElementById(x+''+y+_name);
+        targetTile.textContent = "X";
+        targetTile.style.color = "red";
+    }
+
+    const missSelf = (x, y) => {
+        let targetTile = document.getElementById(x+''+y+_name);
+        targetTile.textContent = "X";
+        targetTile.style.color = "white";
+    }
+
+    return {
+        initialize,
+        addShip,
+        finishPlacement,
+        hitOpponent,
+        missOpponent,
+        hitSelf,
+        missSelf
+        //showboard
+        //hideboard
+    }
 }
--- script.js
+++ script.js
@@ -5,8 +5,10 @@ import { drawBoard } from "./interface.js";
 let name = 'humanNameHere'
 let player1 = Player(name)
 let interface1 = drawBoard(name)
+interface1.initialize()
 
 let player2 = Player()//not including a name identifies a bot
+let botGame = true;
 
 let carrier = Ship(5);
 let battleship = Ship(4);
@@ -22,26 +24,43 @@ let destroyer2 = Ship(2);
 
 //manually input ship coordinates (handled by DOM later)
 player1.getBoard().placeShip(1, 1, carrier)
+interface1.addShip(1, 1, 'c')
 player1.getBoard().placeShip(2, 1, carrier)
+interface1.addShip(2, 1, 'c')
 player1.getBoard().placeShip(3, 1, carrier)
+interface1.addShip(3, 1, 'c')
 player1.getBoard().placeShip(4, 1, carrier)
+interface1.addShip(4, 1, 'c')
 player1.getBoard().placeShip(5, 1, carrier)
+interface1.addShip(5, 1, 'c')
 
 player1.getBoard().placeShip(8, 1, battleship)
+interface1.addShip(8, 1, 'b')
 player1.getBoard().placeShip(8, 2, battleship)
+interface1.addShip(8, 2, 'b')
 player1.getBoard().placeShip(8, 3, battleship)
+interface1.addShip(8, 3, 'b')
 player1.getBoard().placeShip(8, 4, battleship)
+interface1.addShip(8, 4, 'b')
 
 player1.getBoard().placeShip(3, 3, cruiser)
+interface1.addShip(3, 3, 'r')
 player1.getBoard().placeShip(4, 3, cruiser)
+interface1.addShip(4, 3, 'r')
 player1.getBoard().placeShip(5, 3, cruiser)
+interface1.addShip(5, 3, 'r')
 
 player1.getBoard().placeShip(2, 5, submarine)
+interface1.addShip(2, 5, 's')
 player1.getBoard().placeShip(2, 6, submarine)
+interface1.addShip(2, 6, 's')
 player1.getBoard().placeShip(2, 7, submarine)
+interface1.addShip(2, 7, 's')
 
 player1.getBoard().placeShip(6, 7, destroyer)
+interface1.addShip(6, 7, 'd')
 player1.getBoard().placeShip(7, 7, destroyer)
+interface1.addShip(7, 7, 'd')
 //player2 now
 player2.getBoard().placeShip(1, 1, carrier2)
 player2.getBoard().placeShip(2, 1, carrier2)
@@ -65,8 +84,42 @@ player2.getBoard().placeShip(2, 7, submarine2)
 player2.getBoard().placeShip(6, 7, destroyer2)
 player2.getBoard().placeShip(7, 7, destroyer2)
 
+interface1.finishPlacement();
+initTargets();
 //update interface only when logical state changes, not in logic
-//save input
 //place ship or target location with input
-//update interface with input. if targeting use results from target return value
+//update targeting use results from target return value
 //destroy event listener on interface change
+
+//after placement phase
+
+//combat phase
+// while(!player1.getBoard().fleetIsSunk() && !player2.getBoard().fleetIsSunk()){
+
+// }
+function initTargets(){
+    document.querySelectorAll(".target").forEach(tile => {
+        tile.addEventListener('click', barrage)
+    })
+}
+
+function barrage(e){
+    let targetID = e.target.id;
+    let x = targetID[0];
+    let y = targetID[1];
+
+    let result = player2.getBoard().receiveAttack(x, y);
+    if (result === undefined) interface1.missOpponent(x, y);
+    if (result === false || result === true) interface1.hitOpponent(x, y);
+    document.getElementById(e.target.id).removeEventListener('click', barrage);
+
+    if(botGame) botBarrage();
+    //else hide current interface
+}
+
+let botShots = [];
+function botBarrage(){
+    //fires at random, eventually will implement intelligence
+    let x = Math.floor(Math.random()*10);
+    let y = Math.floor(Math.random()*10);
+}
